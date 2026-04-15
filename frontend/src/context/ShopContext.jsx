cfadca215deb441fd373ba9b3faf20fbda9711ef
HIJAZ <muhdhijaz10@gmail.com>
2026-04-15 09:14:12 +0100

    Shop context in progress

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,11 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets/assets";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
-  const value = { products, currency, delivery_fee };
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(true);
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    showSearch,
+    setShowSearch,
+    search,
+    setSearch,
+  };
 
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
