65aeca61fcf64ea82243cd92fb8fce5361e9f13a
HIJAZ <muhdhijaz10@gmail.com>
2026-04-15 14:54:26 +0100

    shopcontext updated

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState } from "react";
 import { products } from "../assets/assets/assets";
+import { toast } from "react-toastify";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
@@ -7,6 +8,61 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(true);
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId, size) => {
+    if (!size) {
+      toast.error("Select Product Size");
+      return;
+    }
+
+    setCartItems((prev) => {
+      const updated = structuredClone(prev);
+
+      if (updated[itemId]) {
+        if (updated[itemId][size]) {
+          updated[itemId][size] += 1;
+        } else {
+          updated[itemId][size] = 1;
+        }
+      } else {
+        updated[itemId] = { [size]: 1 };
+      }
+
+      return updated;
+    });
+  };
+
+  const getCartCount = () => {
+    let count = 0;
+    for (const itemId in cartItems) {
+      for (const size in cartItems[itemId]) {
+        count += cartItems[itemId][size];
+      }
+    }
+    return count;
+  };
+
+  const updateQuantity = (itemId, size, quantity) => {
+    setCartItems((prev) => {
+      const updated = structuredClone(prev);
+      updated[itemId][size] = quantity;
+      return updated;
+    });
+  };
+
+  const getCartAmount = () => {
+    let total = 0;
+    for (const itemId in cartItems) {
+      const product = products.find((p) => p._id === itemId);
+      if (!product) continue;
+      for (const size in cartItems[itemId]) {
+        total += product.price * cartItems[itemId][size];
+      }
+    }
+    return total;
+  };
+
   const value = {
     products,
     currency,
@@ -15,6 +71,11 @@ const ShopContextProvider = (props) => {
     setShowSearch,
     search,
     setSearch,
+    cartItems,
+    addToCart,
+    getCartCount,
+    updateQuantity,
+    getCartAmount,
   };
 
   return (
