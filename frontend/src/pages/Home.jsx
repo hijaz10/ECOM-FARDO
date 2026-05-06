564abf83b300d829343d995eb081cd2642c7a2cd
HIJAZ <muhdhijaz10@gmail.com>
2026-05-06 10:26:13 +0100

    Home page Updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,13 +4,16 @@ import LatestCollection from "../components/LatestCollection";
 import BestSeller from "../components/BestSeller";
 import OurPolicy from "../components/OurPolicy";
 import NewsLetter from "../components/NewsLetter";
+import Carousel from "../components/Carousel";
 
 function Home() {
   return (
     <div>
-      <Hero />
-      <LatestCollection />
+      <div className="-mx-4 sm:-mx-[3vw] md:-mx-[4vw] lg:-mx-[5vw] -mt-16">
+        <Hero />
+      </div>
       <BestSeller />
+      <LatestCollection />
       <OurPolicy />
       <NewsLetter />
     </div>
