96167f3d31d184991c529e17357de82c3b6a991e
HIJAZ <muhdhijaz10@gmail.com>
2026-05-20 07:16:12 +0100

    Toast configured and error corrected

---
+++ frontend/src/utils/toast.js
@@ -0,0 +1,13 @@
+import { toast as hotToast } from "react-hot-toast";
+
+// mimic react-toastify EXACT API
+const toast = {
+  success: (msg) => hotToast.success(msg),
+  error: (msg) => hotToast.error(msg),
+  info: (msg) => hotToast(msg),
+  warning: (msg) => hotToast(msg),
+  loading: (msg) => hotToast.loading(msg),
+  dismiss: (id) => hotToast.dismiss(id),
+};
+
+export default toast;
