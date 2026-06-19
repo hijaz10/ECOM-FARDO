212dca476820c69604c260b40a1355ab98a07e7a
HIJAZ <muhdhijaz10@gmail.com>
2026-06-19 16:41:00 +0100

    Fix assets import

--- frontend/src/assets/assets/assets.js
+++ frontend/src/assets/assets/assets.js
@@ -1,4 +1,3 @@
-import hero_img from "./hero_img.png";
 import cart_icon from "./cart_icon.png";
 import bin_icon from "./bin_icon.png";
 import dropdown_icon from "./dropdown_icon.png";
@@ -8,16 +7,11 @@ import quality_icon from "./quality_icon.png";
 import search_icon from "./search_icon.png";
 import star_dull_icon from "./star_dull_icon.png";
 import star_icon from "./star_icon.png";
-import support_img from "./support_img.png";
 import menu_icon from "./menu_icon.png";
-import about_img from "./about_img.png";
-import contact_img from "./contact_img.png";
 import stripe_logo from "./stripe_logo.png";
 import cross_icon from "./cross_icon.png";
 
 export const assets = {
-  logo,
-  hero_img,
   cart_icon,
   dropdown_icon,
   exchange_icon,
@@ -27,11 +21,7 @@ export const assets = {
   star_dull_icon,
   star_icon,
   bin_icon,
-  support_img,
   menu_icon,
-  about_img,
-  contact_img,
-  razorpay_logo,
   stripe_logo,
   cross_icon,
 };
