6e373178b3e43b0c7e376158a6e94e383666152f
HIJAZ <muhdhijaz10@gmail.com>
2026-06-22 09:39:03 +0100

    add sitemap for google search console

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: { port: 5173 },
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    sitemap({
+      hostname: "https://fardocosmetics.com",
+    }),
+  ],
+
+  server: {
+    port: 5173,
+  },
 });
